f98bb6eee6a0162fadc9015afd84ec8c10da857a
marco <marco.sterbik@protonmail.com>
2026-09-02 23:06:30 +0200

    ui: try change colorscheme

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,7 @@ fn main() -> anyhow::Result<()> {
     let app = Rc::new(RefCell::new(App::new(repo)));
 
     let ui = AppWindow::new()?;
+    ui.invoke_set_light_theme();
 
     refresh_cards(&ui, &app.borrow());
     refresh_sets(&ui, &app.borrow());
